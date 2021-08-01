fae2983d1236bb1d2b5aa8b74d875ea236676c90
TetianaZelena <tanydans@gmail.com>
2021-08-01 14:57:28 +0300

    add

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import eventsTemplatesTumb from './templates/events_tumb';
 import API from './js/api-service'
 import refsGet from './js/refs';
 
+const newAPI =  API.feachImages();
+
 const refs = refsGet();
 
 let valueForm = '';
@@ -16,23 +18,25 @@ refs.searchForm.addEventListener('submit', onSearch);
 
 function onSearch(e) {
    e.preventDefault();
-   const form = e.currentTarget;
-   form.reset();
-   const valueForm = form.elements.searchQuery.value.trim();
    
-    try {
+   const form = e.currentTarget;
+   const valueForm = form.elements.searchQuery.value.trim();
+
+   try {
    API.feachImages(valueForm)
       .then(function renderCartEmage(data) {
    
-   data.hits.forEach(function (hit, totalHits ) {
-     if (valueForm === '') {
+   data.hits.forEach(function (hit) {
+      console.log(hit)
+     
+      if (valueForm === '') {
          clearCartEmage();
          return Notiflix.Notify.warning('ops! Nothing is entered!');
       }
-      else  {
-         numberOfPages = Math.ceil(totalHits / 40);  
+      else {
+         // numberOfPages = Math.ceil(totalHits / 40);  
          clearCartEmage();
-         return Notiflix.Notify.success(`Hooray! We found ${hit} images.`),
+         return Notiflix.Notify.success('Hooray! We found ${hit} images.'),
             refs.galleryList.insertAdjacentHTML('beforeend', eventsTemplatesTumb(hit));
       }
 
@@ -47,7 +51,6 @@ function onSearch(e) {
 function clearCartEmage() {
 refs. galleryList.innerHTML = '';
 }
-
 
 // const value = Object.values(hit.tags);
       // console.log(value.join(' '))
