18052488f9334400bec853528f33a8a008259c89
TetianaZelena <tanydans@gmail.com>
2021-08-02 21:27:15 +0300

    added

--- src/index.js
+++ src/index.js
@@ -11,46 +11,65 @@ const newAPI =  API.feachImages();
 const refs = refsGet();
 
 let valueForm = '';
-let numberOfPages = 0;
+let numberOfPages = 1;
 
+// refs.BtnShowMore.classList.add('is-hidden');
+
+ 
 refs.searchForm.addEventListener('submit', onSearch);
-
-
-function onSearch(e) {
-   e.preventDefault();
+refs.BtnShowMore.addEventListener('click', onSearchBTN)
+refs.BtnShowMore.style.visibility = 'hidden';
+function onSearch(evt) {
+   evt.preventDefault();
    
-   const form = e.currentTarget;
-   const valueForm = form.elements.searchQuery.value.trim();
-
+   const form = evt.currentTarget;
+   valueForm = form.elements.searchQuery.value.trim();
+console.log(valueForm)
    try {
-   API.feachImages(valueForm)
-      .then(function renderCartEmage(data) {
-   
-   data.hits.forEach(function (hit) {
-      console.log(hit)
-     
-      if (valueForm === '') {
-         clearCartEmage();
-         return Notiflix.Notify.warning('ops! Nothing is entered!');
-      }
-      else {
-         // numberOfPages = Math.ceil(totalHits / 40);  
-         clearCartEmage();
-         return Notiflix.Notify.success('Hooray! We found ${hit} images.'),
-            refs.galleryList.insertAdjacentHTML('beforeend', eventsTemplatesTumb(hit));
-      }
-
-   })
+   API.feachImages(valueForm, numberOfPages)
+      .then(function renderCartEmage(data ) {
+        
+         data.hits.forEach(function (hits, totalHits) {
+            
+            if (valueForm === '') {
+               clearCartEmage();
+               return Notiflix.Notify.warning('ops! Nothing is entered!');
+            } else if (hits === 0) {
+                  refs.BtnShowMore.style.visibility = 'hidden';;
+                  Notify.failure("We're sorry, but you've reached the end of search results.");
+               } else {
+                  // loadBtnMore();
+                //   numberOfPages = Math.ceil(totalHits / 40);
+                   refs.BtnShowMore.style.visibility = 'visible';
+       
+                  return Notiflix.Notify.success('Hooray! We found ${hit} images.'),
+                     refs.galleryList.insertAdjacentHTML('beforeend', eventsTemplatesTumb(hits));
+               }
+            }) 
 })
 }
    catch (error) {
       console.log(error);
    }
 }
+
+function onSearchBTN(evt) {
+    numberOfPages += 1;
+    onSearch(valueForm, numberOfPages);
+}
    
 function clearCartEmage() {
 refs. galleryList.innerHTML = '';
 }
 
-// const value = Object.values(hit.tags);
-      // console.log(value.join(' '))
+
+// function loadBtnMore(images, totalHits) {
+//      if (images === 0) {
+//          refs.BtnShowMore.style.visibility = 'hidden';;
+//          Notify.failure("We're sorry, but you've reached the end of search results.");
+//      } else {
+//         numberOfPages = Math.ceil(totalHits / 40)
+//         console.log(numberOfPages)
+//          refs.BtnShowMore.style.visibility = 'visible';;;
+//       }
+// }
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,11 +1,12 @@
 const axios = require('axios').default;
 const BASE_URL = 'https://pixabay.com/api/';
 const APIkey = '22674717-9793e14b490cc19f5f21a9bc5';
-
+const filtersAPI = 'image_type=photo&orientation=horizontal&safesearch=true';
 const page = 1
- async function feachImages(page) {
-    const response = await axios.get((`${BASE_URL}?key=${APIkey}&q=&image_type=photo&orientation=horizontal&safesearch=true&&page=${page}&per_page=40`))
-  
+
+ async function feachImages(searchQuery, page) {
+    const response = await axios.get((`${BASE_URL}?key=${APIkey}&q=${searchQuery}&${filtersAPI}&&page=${page}&per_page=40)`))
+
     if (response.data.hits < 1) {
        return;
     }
@@ -16,9 +17,4 @@ const page = 1
    }
 }
  export default { feachImages }
-// $.getJSON(URL, function(data){
-// if (parseInt(data.totalHits) > 0)
-//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-// else
-//     console.log('No hits');
-// });
+
